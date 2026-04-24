28defb598ed48262dceddafdc17d764cce25ff6d
Yanislav Dragiyski <jnislav.dragiiski02@gmail.com>
2026-04-24 11:57:43 +0200

    Repo init

---
+++ src/main/java/projects/smart_grocery/SmartGroceryApplication.java
@@ -0,0 +1,13 @@
+package projects.smart_grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGroceryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGroceryApplication.class, args);
+    }
+
+}
